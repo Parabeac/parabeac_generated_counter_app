9489c1ccabb705e2e33c9d026d2720cb957c37ed
ivanhuerta20 <ivanhuerta20@gmail.com>
2021-09-04 13:48:11 -0500

    Updated README, Added CounterCubit Comments

--- lib/models/counter_cubit.dart
+++ lib/models/counter_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+/// This Cubit was not generated and entirely created by developer(s).
+/// This Cubit is responsible for providing the state values & business logic for the components needed in my_app_screen.g.dart
 class CounterCubit extends Cubit<int> {
   CounterCubit({
     int initialState = 0,
